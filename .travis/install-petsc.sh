bac29d62aadb528ec677fbf7dd6f52714dc76606
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-03-06 19:10:08 -0800

    Adds initial support for Travis-CI and CodeCov

---
+++ .travis/install-petsc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+git clone https://bitbucket.org/petsc/petsc.git
+
+PETSC_GIT_HASH=v3.10.4
+DEBUG=1
+
+cd petsc
+
+git checkout ${PETSC_GIT_HASH}
+
+export PETSC_DIR=$PWD
+
+./configure PETSC_ARCH=petsc-arch \
+--with-cc=gcc \
+--with-cxx=g++ \
+--with-fc=gfortran \
+--CFLAGS='-g -O0' --CXXFLAGS='-g -O0' --FFLAGS='-g -O0 -Wno-unused-function' \
+--with-clanguage=c \
+--with-debug=$DEBUG  \
+--with-shared-libraries=0 \
+--download-hdf5 \
+--download-metis \
+--download-parmetis \
+--download-fblaslapack \
+--download-mpich=http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+
+
+make all
+make test
+
