911dad533650a22aac7a0f1a2edaa1027ea4d5f0
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-10-28 10:54:05 -0700

    Indents a file

--- src/f90-mod/tdycoremod.F90
+++ src/f90-mod/tdycoremod.F90
@@ -4,84 +4,84 @@ module tdycoredefdummy
 end module tdycoredefdummy
 
 module tdycoredef
- use tdycoredefdummy
+  use tdycoredefdummy
 end module tdycoredef
 
 module tdycore
   use tdycoredef
-        interface
-subroutine TDyCreate(a,b,z)
- use petscdm
- use tdycoredef
- DM a
- TDy b
- integer z
-end subroutine TDyCreate
-        end interface
-        interface
-subroutine TDySetDiscretizationMethod(a,b,z)
- use tdycoredef
- TDy a
- PetscInt b
- integer z
-end subroutine TDySetDiscretizationMethod
-        end interface
-        interface
-subroutine TDySetFromOptions(a,z)
- use tdycoredef
- TDy a
- integer z
-end subroutine TDySetFromOptions
-        end interface
-        interface
-subroutine TDyComputeSystem(a,b,c,z)
- use tdycoredef
- use petscvec
- use petscmat
- TDy a
- Mat b
- Vec c
- integer z
-end subroutine TDyComputeSystem
-        end interface
+  interface
+     subroutine TDyCreate(a,b,z)
+       use petscdm
+       use tdycoredef
+       DM a
+       TDy b
+       integer z
+     end subroutine TDyCreate
+  end interface
+  interface
+     subroutine TDySetDiscretizationMethod(a,b,z)
+       use tdycoredef
+       TDy a
+       PetscInt b
+       integer z
+     end subroutine TDySetDiscretizationMethod
+  end interface
+  interface
+     subroutine TDySetFromOptions(a,z)
+       use tdycoredef
+       TDy a
+       integer z
+     end subroutine TDySetFromOptions
+  end interface
+  interface
+     subroutine TDyComputeSystem(a,b,c,z)
+       use tdycoredef
+       use petscvec
+       use petscmat
+       TDy a
+       Mat b
+       Vec c
+       integer z
+     end subroutine TDyComputeSystem
+  end interface
 
-      Interface TDySetResidualSaturationValuesLocal
-        subroutine TDySetResidualSaturationValuesLocal0(a,b,c,d,z)
-          use tdycoredef
-          TDy a ! Vec
-          PetscInt b ! PetscInt
-          PetscInt c (*) ! PetscInt
-          PetscScalar d (*) ! PetscScalar
-          integer z
-        end subroutine
-        subroutine TDySetResidualSaturationValuesLocal11(a,b,c,d,e,z)
-          use tdycoredef
-          TDy a ! TDy
-          PetscInt b ! PetscInt
-          PetscInt c ! PetscInt
-          PetscScalar d ! PetscScalar
-          integer z
-        end subroutine
-      end interface TDySetResidualSaturationValuesLocal
+  Interface TDySetResidualSaturationValuesLocal
+     subroutine TDySetResidualSaturationValuesLocal0(a,b,c,d,z)
+       use tdycoredef
+       TDy a ! Vec
+       PetscInt b ! PetscInt
+       PetscInt c (*) ! PetscInt
+       PetscScalar d (*) ! PetscScalar
+       integer z
+     end subroutine TDySetResidualSaturationValuesLocal0
+     subroutine TDySetResidualSaturationValuesLocal11(a,b,c,d,e,z)
+       use tdycoredef
+       TDy a ! TDy
+       PetscInt b ! PetscInt
+       PetscInt c ! PetscInt
+       PetscScalar d ! PetscScalar
+       integer z
+     end subroutine TDySetResidualSaturationValuesLocal11
+  end interface TDySetResidualSaturationValuesLocal
 
-        interface
-subroutine TDySetIFunction(a,b,z)
- use tdycoredef
- use petscts
- TS a
- TDy b
- integer z
-end subroutine TDySetIFunction
-        end interface
+  interface
+     subroutine TDySetIFunction(a,b,z)
+       use tdycoredef
+       use petscts
+       TS a
+       TDy b
+       integer z
+     end subroutine TDySetIFunction
+  end interface
 
-        interface
-subroutine TDySetIJacobian(a,b,z)
- use tdycoredef
- use petscts
- TS a
- TDy b
- integer z
-end subroutine TDySetIJacobian
-        end interface
+  interface
+     subroutine TDySetIJacobian(a,b,z)
+       use tdycoredef
+       use petscts
+       TS a
+       TDy b
+       integer z
+     end subroutine TDySetIJacobian
+  end interface
 
 end module tdycore
