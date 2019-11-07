3bbb1a36d25741976e6e981c2d8fc4007b311d70
Gautam Bisht <gautam.bisht@pnnl.gov>
2019-11-06 19:16:35 -0800

    Fixes deriv of relative permeability for fully saturated conditions

--- src/tdypermeability.c
+++ src/tdypermeability.c
@@ -126,6 +126,11 @@ PetscErrorCode TDySetCellPermeability(TDy tdy,PetscInt c,PetscReal *K) {
 
 void RelativePermeability_Irmay(PetscReal m,PetscReal Se,PetscReal *Kr,
                                 PetscReal *dKr_dSe) {
+  *Kr = 1.0;
+  if (dKr_dSe) *dKr_dSe = 0.0;
+
+  if (Se>=1.0) return;
+
   *Kr = PetscPowReal(Se,m);
   if(dKr_dSe) *dKr_dSe = PetscPowReal(Se,m-1)*m;
 }
@@ -133,6 +138,12 @@ void RelativePermeability_Irmay(PetscReal m,PetscReal Se,PetscReal *Kr,
 void RelativePermeability_Mualem(PetscReal m,PetscReal Se,PetscReal *Kr,
 				 PetscReal *dKr_dSe) {
   PetscReal Se_one_over_m,tmp;
+
+  *Kr = 1.0;
+  if(dKr_dSe) *dKr_dSe = 0.0;
+
+  if (Se>=1.0) return;
+
   Se_one_over_m = PetscPowReal(Se,1/m);
   tmp = PetscPowReal(1-Se_one_over_m,m);
   (*Kr)  = PetscSqrtReal(Se);
