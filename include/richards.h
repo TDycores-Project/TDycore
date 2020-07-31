0dc9c26887e87326967153b07a7685f431bb7680
Glenn Hammond <glenn.hammond@pnnl.gov>
2020-07-31 14:38:52 -0700

    Added base richards struct

---
+++ include/richards.h
@@ -0,0 +1,22 @@
+#if !defined(RICHARDS_H)
+#define RICHARDS_H
+
+#include <petsc.h>
+#include <petsc/private/petscimpl.h>
+#include <tdycore.h>
+
+typedef struct Richards Richards;
+
+struct Richards {
+  int i;
+  TDy tdy;
+  Vec U;
+  Vec F;
+  Mat J;
+};
+
+PETSC_EXTERN PetscErrorCode RichardsCreate(Richards **);
+PETSC_EXTERN PetscErrorCode RichardsRunToTime(Richards,PetscReal);
+PETSC_EXTERN PetscErrorCode RichardsDestroy(Richards **);
+
+#endif
