f4362148d63044222d94af09dea85f4d09cc7e6a
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-11-03 10:27:05 -0800

    Fixes namespace of functions for tdympfao2Dimpl

--- include/private/tdympfao2Dimpl.h
+++ include/private/tdympfao2Dimpl.h
@@ -3,12 +3,12 @@
 
 #include <petsc.h>
 
-PETSC_EXTERN PetscErrorCode TDyComputeGMatrixFor2DMesh(TDy);
-PETSC_EXTERN PetscErrorCode TDyComputeTransmissibilityMatrix2DMesh(TDy);
-PETSC_EXTERN PetscErrorCode TDyMPFAOComputeSystem_InternalVertices_2DMesh(TDy,Mat,Vec);
-PETSC_EXTERN PetscErrorCode TDyMPFAOComputeSystem_BoundaryVertices_SharedWithInternalVertices_2DMesh(TDy,Mat,Vec);
-PETSC_EXTERN PetscErrorCode TDyMPFAOComputeSystem_BoundaryVertices_NotSharedWithInternalVertices_2DMesh(TDy,Mat,Vec);
-PETSC_EXTERN PetscErrorCode TDyMPFAORecoverVelocity_2DMesh(TDy,Vec);
-PETSC_EXTERN PetscReal TDyMPFAOVelocityNorm_2DMesh(TDy);
+PETSC_INTERN PetscErrorCode TDyComputeGMatrixFor2DMesh(TDy);
+PETSC_INTERN PetscErrorCode TDyComputeTransmissibilityMatrix2DMesh(TDy);
+PETSC_INTERN PetscErrorCode TDyMPFAOComputeSystem_InternalVertices_2DMesh(TDy,Mat,Vec);
+PETSC_INTERN PetscErrorCode TDyMPFAOComputeSystem_BoundaryVertices_SharedWithInternalVertices_2DMesh(TDy,Mat,Vec);
+PETSC_INTERN PetscErrorCode TDyMPFAOComputeSystem_BoundaryVertices_NotSharedWithInternalVertices_2DMesh(TDy,Mat,Vec);
+PETSC_INTERN PetscErrorCode TDyMPFAORecoverVelocity_2DMesh(TDy,Vec);
+PETSC_INTERN PetscReal TDyMPFAOVelocityNorm_2DMesh(TDy);
 
 #endif
