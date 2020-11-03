354c157fee667e9eab846665e7ece42ffa672723
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-11-03 10:42:24 -0800

    Fixes namespace of functions in tdympfao3Dthtsimpl.h

--- include/private/tdympfao3Dthtsimpl.h
+++ include/private/tdympfao3Dthtsimpl.h
@@ -3,12 +3,12 @@
 
 #include <petsc.h>
 
-PETSC_EXTERN PetscErrorCode TDyMPFAOIFunction_InternalVertices_3DMesh_TH(Vec,Vec,void*);
-PETSC_EXTERN PetscErrorCode TDyMPFAOIFunction_BoundaryVertices_SharedWithInternalVertices_3DMesh_TH(Vec,Vec,void*);
-PETSC_EXTERN PetscErrorCode TDyMPFAOIFunction_BoundaryVertices_NotSharedWithInternalVertices_3DMesh_TH(Vec,Vec,void*);
-PETSC_EXTERN PetscErrorCode TDyMPFAOIJacobian_InternalVertices_3DMesh_TH(Vec, Mat,void*);
-PETSC_EXTERN PetscErrorCode TDyMPFAOIJacobian_BoundaryVertices_SharedWithInternalVertices_3DMesh_TH(Vec, Mat,void*);
-PETSC_EXTERN PetscErrorCode TDyMPFAOIJacobian_BoundaryVertices_NotSharedWithInternalVertices_3DMesh_TH(Vec, Mat,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIFunction_InternalVertices_3DMesh_TH(Vec,Vec,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIFunction_BoundaryVertices_SharedWithInternalVertices_3DMesh_TH(Vec,Vec,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIFunction_BoundaryVertices_NotSharedWithInternalVertices_3DMesh_TH(Vec,Vec,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIJacobian_InternalVertices_3DMesh_TH(Vec, Mat,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIJacobian_BoundaryVertices_SharedWithInternalVertices_3DMesh_TH(Vec, Mat,void*);
+PETSC_INTERN PetscErrorCode TDyMPFAOIJacobian_BoundaryVertices_NotSharedWithInternalVertices_3DMesh_TH(Vec, Mat,void*);
 #endif
 
 
