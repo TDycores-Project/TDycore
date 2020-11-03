39df3270cb9630518a9275a69f1282cd90a034d6
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-11-03 11:09:55 -0800

    Fixes namespace of functions in tdyth.h

--- include/tdyth.h
+++ include/tdyth.h
@@ -4,12 +4,12 @@
 #include <petsc.h>
 #include <tdycore.h>
 
-PETSC_EXTERN PetscErrorCode TDyTHInitialize(TDy);
-PETSC_EXTERN PetscErrorCode TDyTHTSPostStep(TS);
-PETSC_EXTERN PetscErrorCode TDyTHSNESPostCheck(SNESLineSearch,Vec,Vec,Vec,
+PETSC_INTERN PetscErrorCode TDyTHInitialize(TDy);
+PETSC_INTERN PetscErrorCode TDyTHTSPostStep(TS);
+PETSC_INTERN PetscErrorCode TDyTHSNESPostCheck(SNESLineSearch,Vec,Vec,Vec,
                                                PetscBool*,PetscBool*,
                                                void*);
-PETSC_EXTERN PetscErrorCode TDyTHConvergenceTest(SNES,PetscInt,PetscReal,
+PETSC_INTERN PetscErrorCode TDyTHConvergenceTest(SNES,PetscInt,PetscReal,
                                                  PetscReal,PetscReal,
                                                  SNESConvergedReason*,void*);
 
