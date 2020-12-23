b79a9fbf5fb817409d4126af46625f2e9a316001
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-12-23 14:25:56 -0800

    Added environment variables for cmocka.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -3,5 +3,11 @@ sudo apt-get update -qq
 #sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
 sudo apt-get install -y cmake gcc gfortran g++
 sudo apt-get install -y netcdf-bin libnetcdf-dev
+
+# Install and identify the location of the cmocka testing library.
+# (The values of the environment variables below don't matter, because
+#  cmocka files are installed in standard locations.)
 sudo apt-get install -y libcmocka-dev
+export CMOCKA_INCLUDE_DIR=/usr/local/include
+export CMOCKA_INCLUDE_DIR=/usr/local/lib
 
