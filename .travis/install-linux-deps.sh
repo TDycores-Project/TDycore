f85cae9ae5efc291e95e72889224a75965f11946
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-12-23 15:55:24 -0800

    Moving CMocka env vars to main Travis script.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -4,10 +4,6 @@ sudo apt-get update -qq
 sudo apt-get install -y cmake gcc gfortran g++
 sudo apt-get install -y netcdf-bin libnetcdf-dev
 
-# Install and identify the location of the cmocka testing library.
-# (The values of the environment variables below don't matter, because
-#  cmocka files are installed in standard locations.)
+# Install the cmocka testing library.
 sudo apt-get install -y libcmocka-dev
-export CMOCKA_INCLUDE_DIR=/usr/local/include
-export CMOCKA_INCLUDE_DIR=/usr/local/lib
 
--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -9,10 +9,8 @@ brew install gcc@7
 #brew install open-mpi
 #brew install netcdf
 
-# Install and identify the location of the cmocka testing library.
+# Install the cmocka testing library.
 brew install cmocka
-export CMOCKA_INCLUDE_DIR=/usr/local/include
-export CMOCKA_LIBRARY_DIR=/usr/local/lib
 
 # Make sure the weird gfortran library links are in place.
 #ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
