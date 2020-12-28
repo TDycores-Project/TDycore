5659e96d7b2ecc666824f54df8496d194281ac5a
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-12-28 09:28:51 -0800

    Removing Travis CI machinery.

--- .travis/install-linux-deps.sh
+++
@@ -1,6 +0,0 @@
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
-#sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
-sudo apt-get install -y cmake gcc gfortran g++
-sudo apt-get install -y netcdf-bin libnetcdf-dev
-
