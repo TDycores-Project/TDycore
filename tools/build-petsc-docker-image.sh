cc5cb321bbb01ccf6d7ba0420de83d837f5dab8f
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-02-17 16:54:18 -0800

    Added some tools to build PETSc-equipped Docker images.

---
+++ tools/build-petsc-docker-image.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script builds a Docker image that contains an installation of PETSc
+# configured specifically for TDycore. PETSc is installed in /opt/haero,
+# on top of a recent Ubuntu image. Run it like so:
+#
+# `./build-petsc-docker-image.sh <petsc-hash>`
+#
+# The arguments are:
+# <petsc-hash> - A Git hash identifying the revision of PETSc to build
+#
+# For this script to work, Docker must be installed on your machine.
+PETSC_HASH=$1
+
+if [[ "$1" == "" ]]; then
+  echo "Usage: $0 <petsc-hash>"
+  exit
+fi
+
+DOCKERHUB_USER=coherellc
+IMAGE_NAME=tdycore-petsc
+TAG=$PETSC_HASH
+
+# Build the image locally.
+mkdir -p docker-build
+cp Dockerfile.petsc docker-build/Dockerfile
+cd docker-build
+docker build -t $IMAGE_NAME:$TAG --network=host \
+  --build-arg PETSC_HASH=$PETSC_HASH \
+  .
+cd ..
+rm -rd docker-build
+
+# Tag the image.
+docker image tag $IMAGE_NAME:$TAG $DOCKERHUB_USER/$IMAGE_NAME:$TAG
+
+echo "To upload this image to DockerHub, use the following:"
+echo "docker login"
+echo "docker image push $DOCKERHUB_USER/$IMAGE_NAME:$TAG"
+echo "docker logout"
