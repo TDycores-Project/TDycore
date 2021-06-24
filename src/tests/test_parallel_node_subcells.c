828bba6e1193f79ff5d2dc67118e11741971ac45
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-06-24 09:30:41 -0700

    Simplified test. We must be close at this point.

--- src/tests/test_parallel_node_subcells.c
+++ src/tests/test_parallel_node_subcells.c
@@ -5,6 +5,11 @@
 #include <petsc/private/kernels/khash.h>
 KHASH_SET_INIT_INT(INT_SET)
 
+#ifdef CHKERRQ
+#undef CHKERRQ
+#endif
+#define CHKERRQ(x) assert_int_equal(0, x)
+
 // This unit test suite tests that each node in a periodic box mesh is connected
 // to exactly 8 subcells in a parallel configuration.
 
@@ -40,115 +45,89 @@ static void TestPeriodicBoxMeshNodeSubcells(void **state)
   // We create an "interpolated" DMPlex, which includes edges and faces in
   // addition to cells and vertices. These various mesh "points" are accessible
   // via "strata" at specific "heights":
+  //
   // Height | Type
   // 0      | cell
   // 1      | face
   // 2      | edge
   // 3      | vertex
-  // Note: An "uninterpolated" mesh is just a cell-vertex mesh without edges and
-  // Note: faces.
-  const PetscBool interpolate = PETSC_FALSE;
+  //
+  // (An "uninterpolated" mesh is just a cell-vertex mesh without edges and
+  //  faces.)
+  const PetscBool interpolate = PETSC_TRUE;
   PetscInt vertex_height = interpolate ? 3 : 1;
+  PetscInt cell_height = 0;
   ierr = DMPlexCreateBoxMesh(comm, dim, PETSC_FALSE, faces, lower, upper,
-                             periodicity, interpolate, &mesh);
-  assert_int_equal(0, ierr);
+                             periodicity, interpolate, &mesh); CHKERRQ(ierr);
 
   // Here we set the adjacency relations for the mesh so that nodes can see
-  // their attached cells.
-  ierr = DMSetBasicAdjacency(mesh, PETSC_TRUE, PETSC_TRUE);
-  assert_int_equal(0, ierr);
+  // their attached cells ("FVM++").
+  ierr = DMSetBasicAdjacency(mesh, PETSC_TRUE, PETSC_TRUE); CHKERRQ(ierr);
 
-  // Distribute the mesh amongst the processes and construct a single layer of
-  // cells overlapping with other processes. Create a distributed star forest so
-  // we can tell which vertices are owned by the local process ("roots").
-  PetscSF sf;
+  // Distribute the mesh amongst all processes and gather the "local" vertices
+  // that appear only on this process.
+  khash_t(INT_SET)* local_vertices = kh_init(INT_SET);
   {
     DM mesh_dist = NULL;
-    PetscSF sf_dist = NULL;
-    ierr = DMPlexDistribute(mesh, 1, &sf, &mesh_dist);
-    assert_int_equal(0, ierr);
+    ierr = DMPlexDistribute(mesh, 0, NULL, &mesh_dist); CHKERRQ(ierr);
     if (mesh_dist != NULL) {
       DMDestroy(&mesh);
       mesh = mesh_dist;
-
-      ierr = DMPlexCreatePointSF(mesh, sf, PETSC_TRUE, &sf_dist);
-      assert_int_equal(0, ierr);
-      if (sf_dist != NULL) {
-        PetscSFDestroy(&sf);
-        sf = sf_dist;
-      }
     }
-    PetscSFSetUp(sf);
+
+    PetscInt v_start, v_end;
+    ierr = DMPlexGetHeightStratum(mesh, vertex_height, &v_start, &v_end); CHKERRQ(ierr);
+    int retval;
+    for (PetscInt v = v_start; v < v_end; ++v) {
+      kh_put(INT_SET, local_vertices, v, &retval);
+    }
   }
 
-  int my_rank;
-  MPI_Comm_rank(PETSC_COMM_WORLD, &my_rank);
-
-  // Get the indices of all mesh points owned by other processes and store them
-  // in a set.
-  khash_t(INT_SET)* remote_points = kh_init(INT_SET);
+  // Add a layer of overlapping cells from other processes.
   {
-    PetscInt nranks, num_leaves;
-    const PetscMPIInt *ranks;
-    const PetscInt *rank_offsets, *local_indices;
-    ierr = PetscSFGetRootRanks(sf, &nranks, &ranks, &rank_offsets,
-                               &local_indices, NULL);
-    if (nranks == 0) {
-    } else {
-      for (int r = 0; r < nranks; ++r) {
-        if (ranks[r] != my_rank) {
-          PetscInt begin = rank_offsets[r];
-          PetscInt end = rank_offsets[r+1];
-          int retval;
-          for (PetscInt i = begin; i < end; ++i) {
-            kh_put(INT_SET, remote_points, local_indices[i], &retval);
-          }
-        }
-      }
+    DM mesh_dist = NULL;
+    ierr = DMPlexDistributeOverlap(mesh, 1, NULL, &mesh_dist); CHKERRQ(ierr);
+    if (mesh_dist != NULL) {
+      DMDestroy(&mesh);
+      mesh = mesh_dist;
     }
   }
 
   // Traverse the **locally owned** vertices of the mesh and check that they are
   // connected to the appropriate cells.
   PetscInt v_start, v_end;
-  ierr = DMPlexGetHeightStratum(mesh, vertex_height, &v_start, &v_end);
-  assert_int_equal(0, ierr);
+  ierr = DMPlexGetHeightStratum(mesh, vertex_height, &v_start, &v_end); CHKERRQ(ierr);
   for (PetscInt v = v_start; v < v_end; ++v) {
 
-    // A vertex is owned locally if it's not in the set of remote points.
-    PetscBool v_is_local = ((kh_size(remote_points) == 0) || !kh_exist(remote_points, v));
+    PetscBool v_is_local = kh_exist(local_vertices, v);
     if (v_is_local) {
-      // Get all points in the transitive closure for this vertex. The point array
-      // is populated here each point and its orientation, so points[2*p] gives
-      // the index of the the pth point.
+      // Get all points in the transitive closure for this vertex.
       PetscInt num_points, *points = NULL;
-      ierr = DMPlexGetTransitiveClosure(mesh, v, PETSC_FALSE, &num_points, &points);
-      assert_int_equal(0, ierr);
+      ierr = DMPlexGetTransitiveClosure(mesh, v, PETSC_FALSE, &num_points, &points); CHKERRQ(ierr);
 
-      // Count all the points that are cells (height == 0).
+      // Count all the points that are cells. The points array is populated with
+      // each point and its orientation, so points[2*p] gives the index of the
+      // pth point.
       PetscInt num_cells = 0;
       for (PetscInt p = 0; p < num_points; ++p) {
         PetscInt height;
         PetscInt point = points[2*p];
-        //      assert_true(point < num_local_points);
-        ierr = DMPlexGetPointHeight(mesh, point, &height);
-        assert_int_equal(0, ierr);
-        if (height == 0) {
+        ierr = DMPlexGetPointHeight(mesh, point, &height); CHKERRQ(ierr);
+        if (height == cell_height) {
           ++num_cells;
         }
       }
 
       // Put our toys away.
-      ierr = DMPlexRestoreTransitiveClosure(mesh, v, PETSC_FALSE, &num_points, &points);
-      assert_int_equal(0, ierr);
+      ierr = DMPlexRestoreTransitiveClosure(mesh, v, PETSC_FALSE, &num_points, &points); CHKERRQ(ierr);
 
+      // Every local vertex should see exactly 8 cells.
       assert_int_equal(8, num_cells);
     }
   }
 
   // Clean up.
-  kh_destroy(INT_SET, remote_points);
-  PetscSFDestroy(&sf);
+  kh_destroy(INT_SET, local_vertices);
   DMDestroy(&mesh);
 }
 
