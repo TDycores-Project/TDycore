daa18c536aa3052988e2d0e45536d6c7fcd792d1
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-06-24 08:43:49 -0700

    make unit-tests now properly reports error statuses.

--- src/tests/run_unit_tests.sh
+++ src/tests/run_unit_tests.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 # This runs unit tests using a one-test-per-process protocol.
-
-count=`$1 count`
-nproc=`$1 nproc`
-i=0
-while [ $i -lt $count ]
+num_failures=0
+for arg in "$@"
 do
-  # Run the test with the appropriate number of MPI processes.
-  $MPIEXEC -n $nproc $1 $i
-  status=$?
-  if test $status -ne 0
-  then
-    echo "Unit test $1 FAILED with status $status."
-    echo "You can run this test with the following command:"
-    echo "$MPIEXEC -np $nproc $1 $i"
-    exit $status
-  fi
-  ((i++))
+  echo "Running $arg"
+  count=`$arg count`
+  nproc=`$arg nproc`
+  i=0
+  while [ $i -lt $count ]
+  do
+    # Run the test with the appropriate number of MPI processes.
+    $MPIEXEC -n $nproc $arg $i
+    status=$?
+    if test $status -ne 0
+    then
+      echo "Unit test $arg FAILED with status $status."
+      echo "You can run this test with the following command:"
+      echo "$MPIEXEC -np $nproc $arg $i"
+      ((num_failures++))
+    fi
+    ((i++))
+  done
 done
+exit $num_failures
