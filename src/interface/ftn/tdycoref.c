c8fb2862a731523c2f1c479d099edb41a5492da5
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-08-03 16:44:22 -0700

    Switched to PETSc one-off Fortran bindings.

--- src/interface/ftn/tdycoref.c
+++ src/interface/ftn/tdycoref.c
@@ -39,6 +39,12 @@
 #define tdysetpermeabilityfunction_                    TDYSETPERMEABILITYFUNCTION
 #define tdysetresidualsaturationfunction_              TDYSETRESIDUALSATURATIONFUNCTION
 #define tdysetforcingfunction_                         TDYSETFORCINGFUNCTION
+#define tdyregisterpressurefn_                         TDYREGISTERPRESSUREFN
+#define tdyregistertemperaturefn_                      TDYREGISTERTEMPERATUREFN
+#define tdyregistervelocityfn_                         TDYREGISTERVELOCITYFN
+#define tdyselectboundarypressurefn_                   TDYSELECTBOUNDARYPRESSUREFN
+#define tdyselectboundarytemperaturefn_                TDYSELECTBOUNDARYTEMPERATUREFN
+#define tdyselectboundaryvelocityfn_                   TDYSELECTBOUNDARYVELOCITYFN
 #define tdysetboundarypressurefn_                      TDYSETBOUNDARYPRESSUREFN
 #define tdysetboundaryvelocityfn_                      TDYSETBOUNDARYVELOCITYFN
 #define tdysetporosityvalueslocal0_                    TDYSETPOROSITYVALUESLOCAL0
@@ -101,6 +107,12 @@
 #define tdysetpermeabilityfunction_                    tdysetpermeabilityfunction
 #define tdysetresidualsaturationfunction_              tdysetresidualsaturationfunction
 #define tdysetforcingfunction_                         tdysetforcingfunction
+#define tdyregisterpressurefn_                         tdyregisterpressurefn
+#define tdyregistertemperaturefn_                      tdyregistertemperaturefn
+#define tdyregistervelocityfn_                         tdyregistervelocityfn
+#define tdyselectboundarypressurefn_                   tdyselectboundarypressurefn
+#define tdyselectboundarytemperaturefn_                tdyselectboundarytemperaturefn
+#define tdyselectboundaryvelocityfn_                   tdyselectboundaryvelocityfn
 #define tdysetboundarypressurefn_                      tdysetboundarypressurefn
 #define tdysetboundaryvelocityfn_                      tdysetboundaryvelocityfn
 #define tdysetporosityvalueslocal0_                    tdysetporosityvalueslocal0
